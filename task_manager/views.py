3f555bee6827cdab0ac0e692cff71f9c9c5b6a2d
Sergei Mikurov <454004@mail.ru>
2023-09-13 02:52:11 +0500

    update templates

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,5 +5,6 @@ class IndexView(TemplateView):
     template_name = 'index.html'
     extra_context = {
         'title': 'Task manager',
+        'app_name': 'Task mager',
         'greetings': 'Hello!',
     }

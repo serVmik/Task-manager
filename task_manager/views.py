56f797a6c2207e6ae569f4b903a456be4c7357ce
Sergei Mikurov <454004@mail.ru>
2023-09-13 21:22:58 +0500

    fix views.py
    fix translation

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,11 @@
 from django.views.generic.base import TemplateView
+from django.utils.translation import gettext_lazy as _
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
     extra_context = {
-        'title': 'Task manager',
-        'text': 'Task manager',
+        'title': _('Task manager'),
+        'text': _('Task manager'),
         'greetings': 'Hello!',
     }

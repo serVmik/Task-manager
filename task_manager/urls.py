b6d2be30159f87960c42138f66b432d1c0374dd4
Sergei Mikurov <454004@mail.ru>
2023-09-13 20:57:47 +0500

    update urls.py
    add i18n_patterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,14 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from .views import HomeView
 
+# https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#the-set-language-redirect-view
 urlpatterns = [
     path('admin/', admin.site.urls),
+] + i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view(), name='home'),
-    path('users/', include('task_manager.users.urls', namespace='users'))
-]
+    path('users/', include('task_manager.users.urls', namespace='users')),
+)

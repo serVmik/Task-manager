e72bb9ab9cdbb6bc15ea21ad87696148f2cd590a
Sergei Mikurov <454004@mail.ru>
2023-09-13 23:19:16 +0500

    update users/views.py
    add UserCreateView
    add UserUpdateView
    add UserDeleteView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,9 +1,31 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.utils.translation import gettext_lazy as _
 
 from .models import AppUser
+from .forms import UserCreateForm, UserUpdateForm
 
 
-class AppUserListView(ListView):
+class UserListView(ListView):
     model = AppUser
     template_name = 'users/list.html'
     context_object_name = 'users'
+
+
+class UserCreateView(CreateView):
+    model = AppUser
+    template_name = 'users/create.html'
+    form_class = UserCreateForm
+    extra_context = {
+        'title': _('Registration')
+    }
+
+
+class UserUpdateView(UpdateView):
+    model = AppUser
+    template_name = 'users/update.html'
+    form_class = UserUpdateForm
+
+
+class UserDeleteView(DeleteView):
+    model = AppUser
+    template_name = 'users/delete.html'

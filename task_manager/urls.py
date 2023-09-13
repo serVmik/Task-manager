4d10d60d75639e6ab3b22a8fc9b275a3ded21baa
Sergei Mikurov <454004@mail.ru>
2023-09-13 23:11:02 +0500

    update urls.py
    add path 'login/'
    add path 'logout/'

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import HomeView
+from .views import HomeView, UserLoginView, logout_user
 
 # https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#the-set-language-redirect-view
 urlpatterns = [
@@ -10,5 +10,7 @@ urlpatterns = [
 ] + i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view(), name='home'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('users/', include('task_manager.users.urls', namespace='users')),
 )

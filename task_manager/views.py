240e588c171ccd8bcbde7a1e79ccf32acdc29c81
Sergei Mikurov <454004@mail.ru>
2023-09-18 22:33:14 +0500

    Fix views.py, add logging and messages

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.base import TemplateView
@@ -16,11 +17,17 @@ class HomeView(TemplateView):
 class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
 
-    success_message = _('Welcome, you are logged in!')
-    if success_message:
-        logger.debug('User is logged in, id_username = ')
-    else:
-        logger.warning('Error authentication')
+    def form_valid(self, form):
+        logger.debug('User is logged in.')
+        response = super().form_valid(form)
+        messages.success(self.request, _('You are logged in'))
+        return response
+
+    def form_invalid(self, form):
+        logger.error('Error authentication')
+        response = super().form_invalid(form)
+        messages.error(self.request, _('Error authentication'))
+        return response
 
 
 class UserLogoutView(SuccessMessageMixin, LogoutView):

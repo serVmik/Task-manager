eeb64e89451b6109d9117d74cf841f062366f902
Sergei Mikurov <454004@mail.ru>
2023-09-21 02:26:20 +0500

    Update tests/test_user_crud.py

--- tests/test_users_crud.py
+++ tests/test_users_crud.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from task_manager.users.models import AppUser
-from task_manager.users.views import UserListView
+from task_manager.users.views import UserListView, UserUpdateView
 
 
 class UserCrudTestCase(TestCase):
@@ -13,19 +13,18 @@ class UserCrudTestCase(TestCase):
             username='ivan_ivanov',
             first_name='Ivan',
             last_name='Ivanov',
-            password='q1s2d3r4',
         )
 
-    def test_model(self):
-        user_ivan = AppUser.objects.get(username='ivan_ivanov')
-        self.assertEquals(user_ivan.first_name, 'Ivan')
-        self.assertEquals(user_ivan.last_name, 'Ivanov')
-        self.assertEquals(user_ivan.get_full_name(), 'Ivan Ivanov')
+    def test_model_user(self):
+        user = AppUser.objects.get(username='ivan_ivanov')
+        self.assertEquals(user.first_name, 'Ivan')
+        self.assertEquals(user.last_name, 'Ivanov')
+        self.assertEquals(user.get_full_name(), 'Ivan Ivanov')
 
-    def test_create(self):
+    def test_create_user(self):
         url_create = reverse('users:create')
-        url_list_users = reverse('users:list')
-        user_petr = {
+        url_list = reverse('users:list')
+        created_user = {
             'username': 'petr_petrov',
             'first_name': 'Petr',
             'last_name': 'Petrov',
@@ -33,21 +32,51 @@ class UserCrudTestCase(TestCase):
             'password2': 'q1s2d3r4',
         }
 
-        self.client.post(url_create, user_petr)
-        response = self.client.get(url_list_users)
+        self.client.post(url_create, created_user)
+        response = self.client.get(url_list)
 
+        # test page
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(url_list_users, '/users/')
+        self.assertEquals(url_list, '/users/')
         self.assertTemplateUsed(response, 'users/list.html')
         self.assertIs(response.resolver_match.func.view_class, UserListView)
 
-        self.assertInHTML('ivan_ivanov', response.content.decode())
-        self.assertInHTML('Ivan Ivanov', response.content.decode())
-        self.assertInHTML('petr_petrov', response.content.decode())
-        self.assertInHTML('Petr Petrov', response.content.decode())
+        # test page content
+        html = response.content.decode()
+        self.assertInHTML('ivan_ivanov', html)
+        self.assertInHTML('Ivan Ivanov', html)
+        self.assertInHTML('petr_petrov', html)
+        self.assertInHTML('Petr Petrov', html)
 
-    # def test_html_update(self):
-    #     pass
+    def test_update_user(self):
+        user = AppUser.objects.get(username='ivan_ivanov')
+        url_update = reverse('users:update', kwargs={'pk': user.pk})
+        self.client.force_login(user)
+
+        # test page
+        response = self.client.get(url_update)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(url_update, f'/users/{user.pk}/update/')
+        self.assertTemplateUsed(response, 'users/form.html')
+        self.assertIs(response.resolver_match.func.view_class, UserUpdateView)
+
+        # test update names
+        new_user_data = {
+            'username': 'ivanov_daryin',
+            'first_name': 'Ivan',
+            'last_name': 'Ivanov-Daryin',
+            'password1': 'r4d3s2q1',
+            'password2': 'r4d3s2q1',
+        }
+        self.client.post(url_update, new_user_data)
+        current_user_data = AppUser.objects.filter(pk=user.pk)
+        [current_user_data] = current_user_data.all().values()
+        for key in ('username', 'first_name', 'last_name'):
+            self.assertEquals(new_user_data[key], current_user_data[key])
+
+        # test update password
+        current_user_data = AppUser.objects.get(pk=user.pk)
+        self.assertTrue(current_user_data.check_password('r4d3s2q1'))
 
     # def test_html_delete(self):
     #     pass

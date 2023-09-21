c631224ea49d072173f61eddb1710677a2df0da6
Sergei Mikurov <454004@mail.ru>
2023-09-21 15:30:11 +0500

    Add AppUserPassesTestMixin in mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,7 +1,21 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 
 class TestUserAuthorizationMixin(UserPassesTestMixin):
 
     def test_func(self):
         return self.request.user.pk == self.get_object().pk
+
+
+class AppUserPassesTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request,
+                       _('Only the owner can edit and delete user.'))
+        return redirect(reverse_lazy('users:list'))

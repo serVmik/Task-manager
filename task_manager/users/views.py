6452eb9c474fce0ada5df1b4b5ac660d2793e63c
Sergei Mikurov <454004@mail.ru>
2023-09-25 13:35:43 +0500

    Refactoring users/views.py
    Add HandleNoPermissionMixin, ModelFormMessagesMixin,
        ModelFormDeleteMessagesMixin

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,16 +1,16 @@
-from django.contrib import messages
-from django.shortcuts import redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
-import logging
-
-from ..mixins import UserPassesTestOwnerMixin, NotLoginRequiredMixin
+from ..mixins import (
+    HandleNoPermissionMixin,
+    UserPassesTestOwnerMixin,
+    ModelFormMessagesMixin,
+    ModelFormDeleteMessagesMixin,
+    NotLoginRequiredMixin,
+)
 from .models import AppUser
 from .forms import UserCreateForm, UserUpdateForm
-
-logger = logging.getLogger('main_log')
 
 
 class ListUsersView(ListView):
@@ -19,28 +19,27 @@ class ListUsersView(ListView):
     template_name = 'users/list.html'
 
 
-class CreateUserView(NotLoginRequiredMixin, CreateView):
+class CreateUserView(
+    ModelFormMessagesMixin,
+    NotLoginRequiredMixin,
+    CreateView,
+):
     form_class = UserCreateForm
     template_name = 'users/form.html'
     success_url = reverse_lazy('login')
     extra_context = {
         'title': _('Registration'),
     }
-
-    def form_valid(self, form):
-        logger.debug('User is registered.')
-        response = super().form_valid(form)
-        messages.success(self.request, _('User is registered.'))
-        return response
-
-    def form_invalid(self, form):
-        logger.error('User registration error.')
-        response = super().form_invalid(form)
-        messages.error(self.request, _('User registration error.'))
-        return response
+    valid_message = 'User is registered.'
+    invalid_message = 'User registration error.'
 
 
-class UpdateUserView(UserPassesTestOwnerMixin, UpdateView):
+class UpdateUserView(
+    HandleNoPermissionMixin,
+    ModelFormMessagesMixin,
+    UserPassesTestOwnerMixin,
+    UpdateView
+):
     model = AppUser
     form_class = UserUpdateForm
     template_name = 'users/form.html'
@@ -49,43 +48,18 @@ class UpdateUserView(UserPassesTestOwnerMixin, UpdateView):
         'title': _('Edit user'),
     }
     raise_exception = False
-
-    def handle_no_permission(self):
-        logger.error('Invalid action.')
-        messages.error(self.request, _('Invalid action.'))
-        return redirect(reverse_lazy('login'))
-
-    def form_valid(self, form):
-        logger.debug('User data updated.')
-        response = super().form_valid(form)
-        messages.success(self.request, _('User data updated.'))
-        return response
-
-    def form_invalid(self, form):
-        logger.error('User update error.')
-        response = super().form_invalid(form)
-        messages.error(self.request, _('User update error.'))
-        return response
+    valid_message = 'User data updated.'
+    invalid_message = 'User update error.'
 
 
-class DeleteUserView(UserPassesTestOwnerMixin, DeleteView):
+class DeleteUserView(
+    HandleNoPermissionMixin,
+    UserPassesTestOwnerMixin,
+    ModelFormDeleteMessagesMixin,
+    DeleteView
+):
     model = AppUser
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users:list')
-
-    def handle_no_permission(self):
-        logger.error('Invalid action.')
-        messages.error(self.request, _('Invalid action.'))
-        return redirect(reverse_lazy('login'))
-
-    def form_valid(self, form):
-        logger.debug('User deleted.')
-        response = super().form_valid(form)
-        messages.success(self.request, _('User deleted.'))
-        return response
-
-    def form_invalid(self, form):
-        logger.error('User deletion error.')
-        response = super().form_invalid(form)
-        messages.error(self.request, _('User deletion error.'))
-        return response
+    valid_message = 'User deleted.'
+    invalid_message = 'User deletion error.'

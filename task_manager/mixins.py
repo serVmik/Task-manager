c0d90c6bf41c6c81609a9c7ef9e4c759c36e9307
Sergei Mikurov <454004@mail.ru>
2023-10-04 01:07:29 +0500

    Update mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,122 +1,74 @@
+import logging
+
 from django.contrib import messages
-from django.contrib.auth.mixins import (
-    AccessMixin,
-    UserPassesTestMixin,
-)
-from django.contrib.messages import get_messages
+from django.contrib.auth.mixins import AccessMixin, UserPassesTestMixin
 from django.shortcuts import redirect
-from django.views.generic import DeleteView
-from django.views.generic.edit import ModelFormMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-
-import logging
 
 logger = logging.getLogger('main_log')
 
 
-class UserPassesTestOwnerMixin(UserPassesTestMixin):
-    """
-    Test whether the user has owner permission.
-
-    Overrides methods django.contrib.auth.mixins.UserPassesTestMixin.
-    """
-
-    def test_func(self):
-        return self.get_object() == self.request.user
-
-    def handle_no_permission(self):
-        messages.error(
-            self.request,
-            _('Only the owner can edit and delete user.')
-        )
-        return redirect(reverse_lazy('users:list'))
-
-
 class NotLoginRequiredMixin(AccessMixin):
-    """
-    Verify that the current user is NOT authenticated.
-    """
+    """Verify that the current user is NOT authenticated."""
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            logger.debug('Invalid action.')
-            messages.error(self.request, _('Invalid action.'))
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
-class HandleNoPermissionMixin(AccessMixin):
+class HandleNoPermissionMixin:
     """
-    Redirects the not authenticated user.
-
-    Overrides method django.contrib.auth.mixins.AccessMixin.
+    Set redirect url and messages,
+    if the user is not authenticated or does not have permission.
     """
+    message_no_permission = 'Invalid action'
+    logger_no_permission = message_no_permission
+    url_no_permission = reverse_lazy('login')
 
     def handle_no_permission(self):
-        logger.error('Invalid action.')
-        messages.error(self.request, _('Invalid action.'))
-        return redirect(reverse_lazy('home'))
+        logger.debug(_(self.logger_no_permission))
+        messages.error(self.request, _(self.message_no_permission))
+        return redirect(self.url_no_permission)
 
 
-class ModelFormMessagesMixin(ModelFormMixin):
-    """
-    Sets the text for log messages.
-    Sets the text for flash messages.
+class OwnerUserPassesTestMixin(UserPassesTestMixin):
+    """Check if the user has owner permission."""
 
-    Use it only with CreateView, UpdateView.
-    Overrides methods django.views.generic.edit.ModelFormMixin.
-    """
+    def owner_check(self):
+        return self.get_object() == self.request.user
 
-    valid_message = ''
-    invalid_message = ''
+    def get_test_func(self):
+        return self.owner_check
+
+
+class AuthorshipTaskCheckMixin(UserPassesTestMixin):
+    """Check current user for authorship of task."""
+
+    def authorship_check(self):
+        author_pk = self.get_object().author.pk
+        current_user_pk = self.request.user.pk
+        return author_pk == current_user_pk
+
+    def get_test_func(self):
+        return self.authorship_check
+
+
+class ModelFormMessagesMixin:
+    """Add a log and a flash messages on form submission."""
+
+    success_message = ''
+    error_message = ''
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        logger.debug(self.valid_message)
-        messages.success(self.request, self.valid_message)
+        logger.debug(self.success_message)
+        messages.success(self.request, self.success_message)
         return response
 
     def form_invalid(self, form):
         response = super().form_invalid(form)
-        logger.error(self.invalid_message)
-        messages.error(self.request, self.invalid_message)
+        logger.error(self.error_message)
+        messages.error(self.request, self.error_message)
         return response
-
-
-class ModelFormDeleteMessagesMixin(DeleteView):
-    """
-    Sets the text for log messages.
-    Sets the text for flash messages.
-
-    Use it only with DeleteView.
-    Overrides methods django.views.generic.DeleteView.
-    """
-
-    valid_message = ''
-    invalid_message = ''
-    protection_message = ''     # unused
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        logger.debug(self.valid_message)
-        messages.success(self.request, self.valid_message)
-        return response
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        logger.error(self.invalid_message)
-        messages.error(self.request, self.invalid_message)
-        return response
-
-
-def test_flash_message(response, expected_message):
-    """
-    Test whether message in response.
-    Test whether message corresponds to the expected message.
-    """
-
-    number_of_messages = 1
-    current_message = get_messages(response.wsgi_request)
-    assert len(current_message) == number_of_messages
-    assert str(*current_message) == expected_message

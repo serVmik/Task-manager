162225fe7b37f24e1e1d523a4d1ca61a0d871cd1
Sergei Mikurov <454004@mail.ru>
2023-10-06 19:14:58 +0500

    Update tests/test_labels_crud.py

--- tests/test_labels_crud.py
+++ tests/test_labels_crud.py
@@ -29,7 +29,7 @@ class TestReadLabelsList(TestCase):
 
     def test_read_labels_list_by_guest(self):
         response = self.client.get(reverse_lazy('labels:list'))
-        self.assertRedirects(response, '/login/', 302)
+        self.assertRedirects(response, reverse_lazy('login'), 302)
         flash_message_test(response, 'You are not authorized')
 
     # add test paginate
@@ -38,6 +38,21 @@ class TestReadLabelsList(TestCase):
 class TestCreateLabel(TestCase):
     fixtures = ['users.json', 'statuses.json', 'labels.json', 'tasks.json']
 
-    # def setUp(self):
-    #     self.author =
+    def setUp(self):
+        self.author = User.objects.get(username='author')
+        self.data_for_label_creation = {
+            "name": "created_label",
+        }
+        self.url = reverse_lazy('labels:create')
 
+    def test_create_data_successfully(self):
+        self.client.force_login(self.author)
+
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Label.objects.filter(name='created_label').exists())
+
+        response = self.client.post(self.url, self.data_for_label_creation)
+        self.assertRedirects(response, reverse_lazy('labels:list'), 302)
+        flash_message_test(response, 'Label created successfully')
+        self.assertTrue(Label.objects.filter(name='created_label').exists())

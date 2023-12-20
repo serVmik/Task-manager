a81cd81f46caf2f69cbffe883753861548d7289b
Sergei Mikurov <454004@mail.ru>
2023-12-20 22:11:43 +0500

    Update testing_functions.py docstring

--- task_manager/tests/testing_functions.py
+++ task_manager/tests/testing_functions.py
@@ -1,12 +1,34 @@
 from django.contrib.messages import get_messages
 
 
-def flash_message_test(response, expected_message):
+def flash_message_test(response, expected_message, number_of_messages=1):
+    """Test flash message in the response.
+
+    Raises AssertionError if the number of messages and the message text in the
+    response not correspond to the specified in function parameters.
+
+    Parameters
+    ----------
+    response : `HttpResponse`
+        Contains a message text (`str`) for the user.
+    expected_message : `str`
+        The message text which must be sent to user in response.
+    number_of_messages : `int`, optional
+        The number of messages which must be sent to user in response
+        (default 1).
+
+    Raises
+    -------
+    AssertionError:
+        Raised if the message text in the response not correspond with
+        expected_message.
+    AssertionError:
+        Raised if the number of messages in the response not correspond with
+        number_of_messages.
     """
-    Test whether message in the response.
-    Test whether message corresponds to the expected message.
-    """
-    number_of_messages = 1
+    # Get message from response.
     current_message = get_messages(response.wsgi_request)
+    # Comparison number of messages.
     assert len(current_message) == number_of_messages
+    # Comparison of message contents.
     assert str(*current_message) == expected_message
